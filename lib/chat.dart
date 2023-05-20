3079af0f7a67e489cee88616a7abcd894962b216
Akash Kumar L <akash4657kumar@gmail.com>
2023-05-20 18:50:37 +0530

    whatsapp -clone

---
+++ lib/chat.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+import 'package:flutter_application_1/whatsapp.dart';
+
+class chat1 extends StatelessWidget{
+  final String name;
+ const chat1({Key?  key,required this.name}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:PreferredSize(
+        preferredSize: Size.fromHeight(100.0),
+         child: AppBar(
+        title:Text(name),backgroundColor: Color.fromARGB(255, 0, 135, 5),
+      ),
+      
+      ),
+      body: SafeArea(
+        child:Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(image: NetworkImage("https://wallpapercave.com/w/wp3998726#google_vignette"), fit: BoxFit.cover),
+            ),
+           child: Column(
+            children:[ 
+              
+            
+            TextField(
+
+              decoration : InputDecoration(
+              border :OutlineInputBorder(),
+              hintText : 'Type something',
+              ),
+            ),IconButton(onPressed:() {
+              Navigator.of(context).pop();
+            }, icon:Icon(Icons.mic),)
+            
+            ],
+         )
+       )
+      )
+    );
+  
+  }
+}
